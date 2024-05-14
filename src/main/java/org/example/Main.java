7689acfa0e3556112f22a9a6087f729b989c81d3
Денис Савельев <denissavelev@MacBook-Pro-Denis.local>
2024-05-14 18:54:35 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        for (long i = 2; i < Long.MAX_VALUE; i++) {
+            if (getDel(i).stream().reduce(0L, Long::sum) == i) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static List<Long> getDel(long d) {
+        List<Long> list = new LinkedList<>();
+
+        return list;
+    }
+}
+
+/**
+ * for (long i = 1; i < d; i++) {
+ *             if (d % i == 0) {
+ *                 list.add(i);
+ *             }
+ *         }
+ */
